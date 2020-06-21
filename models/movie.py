a2809b5fb02f3b6644083cb0341a80669c236bdc
Madlen <madlensarkisian@gmail.com>
2020-06-21 22:38:45 +0300

    [New] Added movie model with some functions

---
+++ models/movie.py
@@ -0,0 +1,38 @@
+from database import DB
+
+class Movie(object):
+    def __init__(self, id, name, genre, release_year, duration, description, rating, director_name):
+        self.id = id
+        self.name = name
+        self.genre = genre
+        self.release_year = release_year
+        self.duration = duration
+        self.description = description
+        self.rating = rating
+        self.director_name = director_name
+
+
+    @staticmethod
+    def all():
+        with DB() as db:
+            movies = db.execute('SELECT * FROM movie').fetchall()
+            return [Movie(*movie) for movie in movies];
+
+    @staticmethod
+    def create(self):
+        with DB() as db:
+            values = (self.name, self.genre, self.release_year, self.duration, self.description, self.rating, self.director_name)
+            db.execute('''
+                INSERT INTO movie (name, genre, release_year, duration, description, rating, director_name)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            ''', values)
+            return self
+
+    @staticmethod
+    def find(id):
+        with DB() as db:
+            movie = db.execute(
+                'SELECT * FROM movie WHERE id = ?',
+                (id,)
+            ).fetchone()
+            return Movie(*movie)
