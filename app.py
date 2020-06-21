61f8776a7b2b44e65dfde1280fbd62d383e96233
Madlen <madlensarkisian@gmail.com>
2020-06-21 22:37:08 +0300

    [Update] Added some routes
    Home route, movies route and add movies route are available

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
 from flask import Flask
+from flask import render_template, request, redirect
+
+from models.movie import Movie
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    return render_template('index.html')
+
+
+@app.route('/movies')
+def list_movies():
+    return render_template('movies.html', movies=Movie.all())
+
+
+@app.route('/movies/add', methods=['GET', 'POST'])
+def add_movie():
+    genres = ['Romance', 'Comedy', 'Action', 'Thriller', 'Drama', 'Fantasy', 'Adventure', 'Crime', 'Sci-fi', 'Historical', 'Horror', 'Mystery', 'Philosophical', 'Political', 'Animation', 'Musical'];
+    if request.method == 'GET':
+        return render_template('add_movie.html', genres=genres)
+    elif request.method == 'POST':
+        movie = Movie(None, request.form['name'],
+                            request.form['genre'],
+                            request.form['release_year'],
+                            request.form['duration'],
+                            request.form['description'],
+                            request.form['rating'],
+                            request.form['director_name']);
+
+        movie.create(movie)
+
+        return redirect('/movies')
+
+if __name__ == '__main__':
+    app.run()
 
